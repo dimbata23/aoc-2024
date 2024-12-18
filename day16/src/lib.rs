0c5e157ca383d916261ad10a5a6c8ef2ad2b70a2
dimbata23 <adimitrov23@outlook.com>
2024-12-18 17:21:57 +0200

    Updated day 16 solution

--- day16/src/lib.rs
+++ day16/src/lib.rs
@@ -26,32 +26,14 @@ fn calculate_part2(input: &Maze) -> usize {
     let (dists, parents) = calc_min_price_to_end(&input);
     let min_dist = min_from_all_dirs(&dists, input.target_pos);
     count_best_spots(&parents, input.target_pos, &dists, min_dist)
-    //let mut visited_cells = HashSet::new();
-    //for dir in Dir::iter() {
-    //    let mut visited = HashSet::new();
-    //    let curr_dist = dists[&(input.target_pos, dir)];
-    //    if curr_dist != min_dist {
-    //        continue;
-    //    }
-    //    count_best_spots_rec(
-    //        &parents,
-    //        &(input.target_pos, dir),
-    //        &mut visited,
-    //        &mut visited_cells,
-    //    );
-    //}
-    //let mut map = input.map.to_vec();
-    //visited_cells.iter().for_each(|&pos| {
-    //    map[pos.y][pos.x] = 'O';
-    //});
-    //print_2d_map(&map);
-    //visited_cells.len()
 }
 
-fn min_from_all_dirs(dists: &HashMap<(Vec2, Dir), usize>, pos: Vec2) -> usize {
-    let mut min = dists[&(pos, Dir::Right)];
+fn min_from_all_dirs(dists: &HashMap<CellDir, usize>, pos: Vec2) -> usize {
+    let start = CellDir::new(pos, Dir::Right);
+    let mut min = dists[&start];
     for dir in Dir::iter() {
-        let curr_dist = dists[&(pos, dir)];
+        let cell_dir = CellDir::new(pos, dir);
+        let curr_dist = dists[&cell_dir];
         if curr_dist < min {
             min = curr_dist;
         }
@@ -65,54 +47,54 @@ struct Maze {
     target_pos: Vec2,
 }
 
+#[derive(Hash, Eq, PartialEq, Copy, Clone)]
+struct CellDir {
+    pos: Vec2,
+    dir: Dir,
+}
+
 fn count_best_spots(
-    parents: &HashMap<(Vec2, Dir), Vec<(Vec2, Dir)>>,
+    parents: &HashMap<CellDir, Vec<CellDir>>,
     end_pos: Vec2,
-    dists: &HashMap<(Vec2, Dir), usize>,
+    dists: &HashMap<CellDir, usize>,
     min_dist: usize,
 ) -> usize {
     let mut visited_cells = HashSet::new();
     for dir in Dir::iter() {
-        let curr_dist = dists[&(end_pos, dir)];
+        let cell_dir = CellDir::new(end_pos, dir);
+        let curr_dist = dists[&cell_dir];
         if curr_dist != min_dist {
             continue;
         }
         let mut visited = HashSet::new();
-        count_best_spots_rec(parents, &(end_pos, dir), &mut visited, &mut visited_cells);
+        count_best_spots_rec(parents, cell_dir, &mut visited, &mut visited_cells);
     }
     visited_cells.len()
 }
 
 fn count_best_spots_rec(
-    parents: &HashMap<(Vec2, Dir), Vec<(Vec2, Dir)>>,
-    (curr_pos, curr_dir): &(Vec2, Dir),
-    visited: &mut HashSet<(Vec2, Dir)>,
+    parents: &HashMap<CellDir, Vec<CellDir>>,
+    cell_dir: CellDir,
+    visited: &mut HashSet<CellDir>,
     visited_cells: &mut HashSet<Vec2>,
 ) {
-    visited_cells.insert(*curr_pos);
+    visited_cells.insert(cell_dir.pos);
 
-    if !visited.insert((*curr_pos, *curr_dir)) {
+    if !visited.insert(cell_dir) {
         return;
     }
 
-    if let Some(curr_parents) = parents.get(&(*curr_pos, *curr_dir)) {
-        for parent in curr_parents {
+    if let Some(curr_parents) = parents.get(&cell_dir) {
+        for &parent in curr_parents {
             count_best_spots_rec(parents, parent, visited, visited_cells);
         }
     }
 }
 
-fn calc_min_price_to_end(
-    maze: &Maze,
-) -> (
-    HashMap<(Vec2, Dir), usize>,
-    HashMap<(Vec2, Dir), Vec<(Vec2, Dir)>>,
-) {
+fn calc_min_price_to_end(maze: &Maze) -> (HashMap<CellDir, usize>, HashMap<CellDir, Vec<CellDir>>) {
     let mut distances = HashMap::new();
     let mut parents = HashMap::new();
     let mut heap = BinaryHeap::new();
-
-    let mut debug_map = maze.map.to_vec();
 
     // push all non-wall cells
     for (y, line) in maze.map.iter().enumerate() {
@@ -121,13 +103,14 @@ fn calc_min_price_to_end(
                 continue;
             }
             for dir in Dir::iter() {
-                distances.insert((Vec2::new(x, y), dir), usize::MAX);
+                distances.insert(CellDir::new(Vec2::new(x, y), dir), usize::MAX);
             }
         }
     }
 
     // start 0 dist
-    if let Some(dist) = distances.get_mut(&(maze.pos, Dir::Right)) {
+    let start_cell_dir = CellDir::new(maze.pos, Dir::Right);
+    if let Some(dist) = distances.get_mut(&start_cell_dir) {
         *dist = 0;
     }
 
@@ -139,59 +122,29 @@ fn calc_min_price_to_end(
     });
 
     while let Some(curr) = heap.pop() {
-        if curr.dist > distances[&(curr.pos, curr.dir)] {
+        let curr_cell_dir = CellDir::new(curr.pos, curr.dir);
+        if curr.dist > distances[&curr_cell_dir] {
             continue;
         }
 
         let neighbours = gen_neighbours(&maze.map, &curr);
         for neighbour in neighbours {
-            let cmp_dist = neighbour
-                .dist
-                .cmp(&distances[&(neighbour.pos, neighbour.dir)]);
-            let is_less = cmp_dist == std::cmp::Ordering::Less;
-            let is_eq = cmp_dist == std::cmp::Ordering::Equal;
+            let neighbour_cell_dir = CellDir::new(neighbour.pos, neighbour.dir);
+            let this_dist = neighbour.dist;
+            let other_dist = distances[&neighbour_cell_dir];
 
-            //if neighbour.pos == Vec2::new(5, 7) {
-            //    println!(
-            //        "[5, 7] dist from {:?}: {}, cmp against {}",
-            //        curr.pos,
-            //        distances[&(neighbour.pos, neighbour.dir)],
-            //        neighbour.dist
-            //    );
-            //}
-
-            if is_less {
-                distances.insert((neighbour.pos, neighbour.dir), neighbour.dist);
-                parents.insert((neighbour.pos, neighbour.dir), vec![(curr.pos, curr.dir)]);
+            if this_dist < other_dist {
+                distances.insert(neighbour_cell_dir, neighbour.dist);
+                parents.insert(neighbour_cell_dir, vec![curr_cell_dir]);
                 heap.push(neighbour);
-                debug_map[neighbour.pos.y][neighbour.pos.x] = 'O';
             }
-            if is_eq {
-                let neigh_parents = parents.entry((neighbour.pos, neighbour.dir)).or_default();
-                neigh_parents.push((curr.pos, curr.dir));
-                debug_map[neighbour.pos.y][neighbour.pos.x] = '=';
-                //println!("eq {:?} -> parent: {:?}", neighbour.pos, neigh_parents);
+
+            if this_dist == other_dist {
+                let neigh_parents = parents.entry(neighbour_cell_dir).or_default();
+                neigh_parents.push(curr_cell_dir);
             }
         }
-
-        debug_map[curr.pos.y][curr.pos.x] = 'X';
-
-        //print_2d_map(&debug_map);
-        //press_enter_to_continue();
     }
-
-    //let debug_parents: HashMap<Vec2, Vec<Vec2>> = parents
-    //    .iter()
-    //    .map(|(pos, parents)| (pos.0, parents.iter().map(|x| x.0).collect::<Vec<Vec2>>()))
-    //    .collect();
-    //
-    //for (pos, parents) in &debug_parents {
-    //    if parents.len() == 1 {
-    //        continue;
-    //    }
-    //
-    //    println!("{:?} <- {:?}", pos, parents);
-    //}
 
     (distances, parents)
 }
@@ -275,6 +228,12 @@ impl PartialOrd for MinPriorityPos {
     }
 }
 
+impl CellDir {
+    fn new(pos: Vec2, dir: Dir) -> Self {
+        Self { pos, dir }
+    }
+}
+
 fn parse_file(file_path: &str) -> io::Result<Maze> {
     let map = parse_2d_map(file_path)?;
     let mut start = Vec2::new(1, 1);
@@ -296,12 +255,3 @@ fn parse_file(file_path: &str) -> io::Result<Maze> {
         target_pos: target,
     })
 }
-
-//fn press_enter_to_continue() {
-//    println!("Press Enter to continue...");
-//    // Flush stdout to ensure the prompt is displayed immediately.
-//
-//    // Read a line from standard input, but ignore the result.
-//    let mut input = String::new();
-//    io::stdin().read_line(&mut input).unwrap();
-//}
