c5952c871ea7094ad836e294dba54b313ebb2392
dimbata23 <adimitrov23@outlook.com>
2024-12-26 20:20:01 +0200

    Added day24 part2 solution

--- day24/src/lib.rs
+++ day24/src/lib.rs
@@ -20,12 +20,13 @@ fn calculate_part1(input: &State) -> u64 {
 }
 
 fn calculate_part2(input: &State) -> String {
-    "".to_string()
+    let broken_outputs = input.get_broken_outputs();
+    broken_outputs.join(",")
 }
 
 type Wire = String;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Gate {
     AND,
     OR,
@@ -86,6 +87,136 @@ impl State {
             }
         }
     }
+
+    fn get_broken_outputs(&self) -> Vec<String> {
+        // z00, z01, z02 are different and it was quicker to check them manually instead of writing code cases
+        let mut vec = vec![];
+        for bit in 3..64 {
+            if let Some(broken_output) = self.get_broken_full_adder_output(bit) {
+                vec.push(broken_output);
+            }
+        }
+        vec.sort();
+        vec
+    }
+
+    fn get_broken_full_adder_output(&self, bit: usize) -> Option<String> {
+        let zbit = str_bit('z', bit);
+        if let None = &self.connections.get(&zbit) {
+            return None;
+        }
+
+        if let None = &self.wire_values.get(&str_bit('x', bit)) {
+            return None; // Hopefully the erorr isn't in the last bit :P
+        }
+
+        let xor_con = &self.connections[&zbit];
+        let prev_xor_con = &self.connections[&zbit];
+        if xor_con.gate != Gate::XOR {
+            return Some(zbit);
+        }
+        assert!(prev_xor_con.gate == Gate::XOR);
+
+        let prev_xor_wires = get_sorted_froms(prev_xor_con);
+
+        match self.check_get_or_from_first_xor(xor_con, bit) {
+            Err(broken_output) => return Some(broken_output),
+            Ok((or_lhs, or_rhs)) => {
+                let lhs_con = &self.connections[or_lhs];
+                let rhs_con = &self.connections[or_rhs];
+                // Input never has both of these swapped
+                // if lhs_con.gate != Gate::AND && rhs_con.gate != Gate::AND {
+                //     return Some(format!("{or_lhs}&{or_rhs}"));
+                // }
+                if lhs_con.gate != Gate::AND {
+                    return Some(or_lhs.to_string());
+                }
+
+                if rhs_con.gate != Gate::AND {
+                    return Some(or_rhs.to_string());
+                }
+
+                let lhs_or_wires = get_sorted_froms(lhs_con);
+                let rhs_or_wires = get_sorted_froms(rhs_con);
+
+                let prev_inputs = (&str_bit('x', bit - 1), &str_bit('y', bit - 1));
+                if lhs_or_wires == prev_xor_wires {
+                    if rhs_or_wires != prev_inputs {
+                        return Some(or_rhs.to_string());
+                    }
+                }
+
+                if rhs_or_wires == prev_xor_wires {
+                    if lhs_or_wires != prev_inputs {
+                        return Some(or_lhs.to_string());
+                    }
+                };
+
+                return None;
+            }
+        }
+    }
+
+    fn check_get_or_from_first_xor(
+        &self,
+        first_xor_conn: &Connection,
+        bit: usize,
+    ) -> Result<(&String, &String), String> {
+        let (xor_lhs, xor_rhs) = get_sorted_froms(first_xor_conn);
+        let lhs_conn = &self.connections[xor_lhs];
+        let rhs_conn = &self.connections[xor_rhs];
+        let mut or_err = "".to_string();
+
+        let mut xor_conn = None;
+        let mut or_conn = None;
+        if lhs_conn.gate == Gate::XOR {
+            let froms = get_sorted_froms(lhs_conn);
+            if froms.0 == &str_bit('x', bit) || froms.1 == &str_bit('y', bit) {
+                xor_conn = Some(lhs_conn);
+                or_conn = Some(rhs_conn);
+                or_err = xor_rhs.to_string();
+            }
+        }
+
+        if xor_conn.is_none() {
+            if rhs_conn.gate == Gate::XOR {
+                let froms = get_sorted_froms(rhs_conn);
+                if froms.0 == &str_bit('x', bit) || froms.1 == &str_bit('y', bit) {
+                    xor_conn = Some(rhs_conn);
+                    or_conn = Some(lhs_conn);
+                    or_err = xor_lhs.to_string();
+                }
+            }
+        }
+
+        if xor_conn.is_none() {
+            if lhs_conn.gate == Gate::OR {
+                return Err(xor_rhs.to_string());
+            }
+            // Input never has both of these swapped
+            /*else if rhs_conn.gate == Gate::OR*/
+            return Err(xor_lhs.to_string());
+
+            // return Err(format!("{xor_lhs}&{xor_rhs}"));
+        }
+
+        let or_conn = or_conn.unwrap();
+        if or_conn.gate == Gate::OR {
+            return Ok(get_sorted_froms(or_conn));
+        }
+
+        Err(or_err)
+    }
+}
+
+fn get_sorted_froms(conn: &Connection) -> (&String, &String) {
+    let mut froms = vec![&conn.from.0, &conn.from.1];
+    froms.sort();
+    (froms[0], froms[1])
+}
+
+fn str_bit(ch: char, bit: usize) -> String {
+    format!("{ch}{bit:02}")
 }
 
 impl Gate {
